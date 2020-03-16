01d8755c957a569969c49fc0b67d253663548f99
des-yogi <nuevo-design@mail.ru>
2020-03-16 19:41:41 +0200

    index-page - fin

--- src/blocks/full-width-slider/full-width-slider.js
+++ src/blocks/full-width-slider/full-width-slider.js
@@ -1,4 +1,25 @@
-// document.addEventListener('DOMContentLoaded', function(){});
-// (function(){
-// код
-// }());
+(function(){
+  var indexFull = new Swiper('.full-width-slider', {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    //loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.2
+      },
+      768: {
+        slidesPerView: 1.55
+      },
+      1366: {
+        slidesPerView: 1.4
+      },
+      1920: {
+        slidesPerView: 1.5
+      }
+    }
+  });
+}());
