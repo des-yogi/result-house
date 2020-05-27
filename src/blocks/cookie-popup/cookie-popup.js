34512a2c1df0a714d18b261d5b7aa726661505ef
des-yogi <nuevo-design@mail.ru>
2020-05-27 15:22:49 +0300

    catalog-block, badges, cookie-consent etc

---
+++ src/blocks/cookie-popup/cookie-popup.js
@@ -0,0 +1,22 @@
+(function() {
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#ffffff",
+        "text": "#090b12"
+      },
+      "button": {
+        "background": "#ffffff",
+        "border": "#ff0000",
+        "text": "#090b12" //secondary-color
+      }
+    },
+    "position": "bottom-right",
+    "content": {
+      "message": "Этот сайт использует файлы cookie в соответствии с <a href='https://www.google.com/' target='_blank'>Политикой использования файлов cookie</a>. Если Вы согласны на использование нами файлов cookie, то нажмите кнопку «Принять».",
+      "dismiss": "Принять",
+      "link": "Подробнее",
+      "href": "https://www.google.com/"
+    }
+  });
+})();
